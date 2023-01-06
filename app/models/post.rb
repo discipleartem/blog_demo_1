4f303e0cb76675cfabbd3607c2ba0309d063864f
Tomas <discipleartem@gmail.com>
2023-01-06 12:20:10 +0200

    add devise user registration and session

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximimum: 50 }
   validates :body, presence: true, length: { minimum: 10, maximimum: 1000 }
 
+  belongs_to :user
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(email: 'my_user@mail.com', password: 'password', password_confirmation: 'password')
+
 10.times do |t|
-  Post.create(title: "My Post №#{t}", body: "This is body for post №#{t}")
+  Post.create(title: "My Post №#{t}", body: "This is body for post №#{t}", user_id: User.first.id)
 end
